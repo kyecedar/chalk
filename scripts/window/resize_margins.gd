d72bebfc2ab1fa8f638b248098b2caac9fd1ac95
Kye Cedar <kyedoart@protonmail.com>
2023-04-10 04:38:47 -0700

    resize margin nodes.

---
+++ scripts/window/resize_margins.gd
@@ -0,0 +1,63 @@
+extends Control
+
+
+
+@onready var top    := $top
+@onready var bottom := $bottom
+@onready var left   := $left
+@onready var right  := $right
+
+@onready var tl := $tl
+@onready var tr := $tr
+@onready var bl := $bl
+@onready var br := $br
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func drag():
+	pass
+
+
+
+
+func _on_top_gui_input(event):
+	pass # Replace with function body.
+
+
+func _on_bottom_gui_input(event):
+	pass # Replace with function body.
+
+
+func _on_left_gui_input(event):
+	pass # Replace with function body.
+
+
+func _on_right_gui_input(event):
+	pass # Replace with function body.
+
+
+func _on_tl_gui_input(event):
+	pass # Replace with function body.
+
+
+func _on_tr_gui_input(event):
+	pass # Replace with function body.
+
+
+func _on_bl_gui_input(event):
+	pass # Replace with function body.
+
+
+func _on_br_gui_input(event):
+	pass # Replace with function body.
--- scripts/window/window.gd
+++ scripts/window/window.gd
@@ -13,6 +13,7 @@ extends Control
 
 @onready var content    := $content
 @onready var background := $background
+@onready var resize     := $resize_margins
 
 var drag_margins := Vector2i(2, 2)
 
@@ -31,3 +32,15 @@ func _on_theme_change() -> void:
 	background.color = chalk.theme.primary
 
 
+
+
+func _on_item_rect_changed():
+	if resize:
+		# Todo, redo. since this can be bypassed by setting the window size to
+		# the screen size and pressing the maximize button.
+		var mode : int = DisplayServer.window_get_mode()
+		
+		if mode == DisplayServer.WINDOW_MODE_FULLSCREEN or mode == DisplayServer.WINDOW_MODE_MAXIMIZED:
+			resize.hide()
+		else:
+			resize.show()
